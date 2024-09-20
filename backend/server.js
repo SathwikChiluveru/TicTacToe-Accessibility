cbe4709e926fa37a264f2646d83311129d05b90e
Sathwik Chiluveru <51926084+SathwikChiluveru@users.noreply.github.com>
2024-09-20 21:03:17 +0800

    feat: socket io backend setup

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 connectDB();
 app.use(cors());
 
+app.use('/api/player', require('./routes/Player.route'));
 
 const httpServer = createServer(app);
 
@@ -18,6 +19,88 @@ const io = new Server(httpServer, {
     cors: {
       origin: "http://localhost:3000"
     }
+});
+
+const allUsers = {};
+const allRooms = [];
+
+io.on("connection", (socket) => {
+  allUsers[socket.id] = {
+    socket: socket,
+    online: true,
+  };
+
+  socket.on("request_to_play", (data) => {
+    const currentUser = allUsers[socket.id];
+    currentUser.playerName = data.playerName;
+
+    let opponentPlayer;
+    let sessionId;
+
+    for (const key in allUsers) {
+      const user = allUsers[key];
+      if (user.online && !user.playing && socket.id !== key) {
+        opponentPlayer = user;
+        break;
+      }
+    }
+
+    if (opponentPlayer) {
+      sessionId = uuidv4();
+
+      allRooms.push({
+        sessionId: sessionId,
+        player1: opponentPlayer,
+        player2: currentUser,
+      });
+
+      currentUser.socket.emit("OpponentFound", {
+        opponentName: opponentPlayer.playerName,
+        playingAs: "circle",
+        sessionId: sessionId,
+      });
+
+      opponentPlayer.socket.emit("OpponentFound", {
+        opponentName: currentUser.playerName,
+        playingAs: "cross",
+        sessionId: sessionId,
+      });
+
+      currentUser.socket.on("playerMoveFromClient", (data) => {
+        opponentPlayer.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+
+      opponentPlayer.socket.on("playerMoveFromClient", (data) => {
+        currentUser.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+    } else {
+      currentUser.socket.emit("OpponentNotFound");
+    }
+  });
+
+  socket.on("disconnect", function () {
+    const currentUser = allUsers[socket.id];
+    currentUser.online = false;
+    currentUser.playing = false;
+
+    for (let index = 0; index < allRooms.length; index++) {
+      const { player1, player2 } = allRooms[index];
+
+      if (player1.socket.id === socket.id) {
+        player2.socket.emit("opponentLeftMatch");
+        break;
+      }
+
+      if (player2.socket.id === socket.id) {
+        player1.socket.emit("opponentLeftMatch");
+        break;
+      }
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
